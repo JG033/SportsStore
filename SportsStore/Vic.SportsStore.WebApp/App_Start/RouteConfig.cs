938442254e779ab34e588704937796e45450f4df
J <J>
2020-08-09 13:10:41 -0400

    [CHG] add paging;

--- SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
+++ SportsStore/Vic.SportsStore.WebApp/App_Start/RouteConfig.cs
@@ -14,10 +14,16 @@ namespace Vic.SportsStore.WebApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Product", action = "List" }
+                );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
         }
     }
 }
--- SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -5,17 +5,33 @@ using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
     public class ProductController : Controller
     {
-        public IProductsRepository ProductsRepository { get; set; } 
+        public IProductsRepository ProductsRepository { get; set; }
+
+        public const int PageSize = 5;
         
-        public ViewResult List()
+        public ViewResult List(int page=1)
         {
-            var model = ProductsRepository.Products;
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
+                .Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
             return View(model);
         }
     }
-}
+} 
